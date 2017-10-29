faec5b3728f4adf85acf055c27eef42808927743
Luis Padron <heyluispadron@gmail.com>
2017-10-29 19:37:43 -0400

    STEP 3. Add get and list methods.

--- Sources/App/Controllers/PokemonController.swift
+++ Sources/App/Controllers/PokemonController.swift
@@ -2,7 +2,6 @@ import Vapor
 import FluentProvider
 
 final class PokemonController {
-
     // "Catches" a pokemon when posting to "pokemons/catch"
     static func catchPokemon(_ req: Request) throws -> ResponseRepresentable {
         guard let json = req.json else {
@@ -16,8 +15,19 @@ final class PokemonController {
         return pokemon
     }
 
+    // Lists all "caught" pokemon when accessing "/"
+    static func listPokemon(_ req: Request) throws -> ResponseRepresentable {
+        return try Pokemon.all().makeJSON()
+    }
+
+    static func getPokemon(_ req: Request) throws -> ResponseRepresentable {
+        return try req.parameters.next(Pokemon.self)
+    }
+
     static func addRoutes(to drop: Droplet) {
         let pokemonGroup = drop.grouped("pokemons")
+        pokemonGroup.get(handler: listPokemon)
+        pokemonGroup.get(Pokemon.parameter, handler: getPokemon)
         pokemonGroup.post("catch", handler: catchPokemon)
     }
 }
